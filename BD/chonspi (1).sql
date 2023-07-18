6effc0898d70949c615bc258f26a72104f2c912e
HikkiiGayya <mcpe951753@gmail.com>
2023-07-17 19:42:22 -0500

    Base de datos

--- BD/chonspi (1).sql
+++ BD/chonspi (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-07-2023 a las 02:35:20
+-- Tiempo de generación: 18-07-2023 a las 02:41:17
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.0.28
 
@@ -20,6 +20,30 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `chonspi`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `id_rol` int(11) NOT NULL,
+  `descripcion` varchar(30) NOT NULL,
+  `estado` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles_vistas`
+--
+
+CREATE TABLE `roles_vistas` (
+  `id_roles_vistas` int(11) NOT NULL,
+  `rol_idRol` int(11) NOT NULL,
+  `vista_idVista` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -44,9 +68,35 @@ CREATE TABLE `usuarios` (
   `respuesta_segurad3` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vistas`
+--
+
+CREATE TABLE `vistas` (
+  `id_vista` int(11) NOT NULL,
+  `url` varchar(45) DEFAULT NULL,
+  `estado` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id_rol`);
+
+--
+-- Indices de la tabla `roles_vistas`
+--
+ALTER TABLE `roles_vistas`
+  ADD PRIMARY KEY (`id_roles_vistas`),
+  ADD KEY `fk_roles_vistas_roles` (`rol_idRol`),
+  ADD KEY `fk_roles_vistas_vistas` (`vista_idVista`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -56,8 +106,26 @@ ALTER TABLE `usuarios`
   ADD KEY `fk_usuarios_rol_id_rol_id` (`rol_id`);
 
 --
+-- Indices de la tabla `vistas`
+--
+ALTER TABLE `vistas`
+  ADD PRIMARY KEY (`id_vista`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `roles_vistas`
+--
+ALTER TABLE `roles_vistas`
+  MODIFY `id_roles_vistas` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
@@ -66,8 +134,21 @@ ALTER TABLE `usuarios`
   MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de la tabla `vistas`
+--
+ALTER TABLE `vistas`
+  MODIFY `id_vista` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `roles_vistas`
+--
+ALTER TABLE `roles_vistas`
+  ADD CONSTRAINT `fk_roles_vistas_roles` FOREIGN KEY (`rol_idRol`) REFERENCES `roles` (`id_rol`),
+  ADD CONSTRAINT `fk_roles_vistas_vistas` FOREIGN KEY (`vista_idVista`) REFERENCES `vistas` (`id_vista`);
 
 --
 -- Filtros para la tabla `usuarios`
